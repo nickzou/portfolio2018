789a0c76562190d06ad10012e2e6d35ee046fd37
Nick Zou <z.nick00@gmail.com>
2018-01-01 21:31:30 -0500

    More set up changes.

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
   function isfahan_resources() {
     wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css');
     wp_enqueue_style('isfahan', get_template_directory_uri().'/css/isfahan.css');
     wp_enqueue_script('script', get_template_directory_uri().'/js/bundle.js');
   }
 
   add_action('wp_enqueue_scripts', 'isfahan_resources');
-
 ?>
--- header.php
+++ header.php
@@ -8,5 +8,13 @@
   </head>
   <body <?php body_class(); ?>>
     <header>
-      <a href="<?php echo home_url(); ?>"><h1><?php bloginfo('name'); ?></h1></a>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col col-12">
+            <section class="header">
+              <a href="<?php echo home_url(); ?>"><h1><?php bloginfo('name'); ?></h1></a>
+            </section>
+          </div>
+        </div>
+      </div>
     </header>
